4e1959e2faa585b52dc1ad830864752bf92880e9
YJyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-03-21 21:17:14 +0900

    11

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import './App.css';
-import BarPlot from './components/BarPlot';
-import BubblePlot from './components/BubblePlot';
-import LinePlot from './components/LinePlot';
-import PiePlot from './components/PiePlot';
-import RadarPlot from './components/RadarPlot';
+import MaterialUI from './components/MaterialUI';
 
 function App() {
   return (
     <div className="App">
-		<LinePlot />
+		{/* <LinePlot />
 		  <PiePlot />
 		  <BarPlot />
 		  <RadarPlot />
-		  <BubblePlot />
+		  <BubblePlot /> */}
+
+		  <MaterialUI />
     </div>
   );
 }
---
+++ src/components/MaterialUI.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@material-ui/core';
+import React from 'react'
+
+
+
+const MaterialUI: React.FC = () =>
+{
+	return (
+		<div>
+		  <Button variant='contained' color="primary">TEST BUTTON</Button>
+		</div>
+	)
+};
+
+export default MaterialUI;
