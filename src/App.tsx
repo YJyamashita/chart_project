a606014d1a8b6d65b5012b0bc433b067fbf4570f
YJyamashita <yuuji.yamashita.yg@east.ntt.co.jp>
2022-03-24 20:50:04 +0900

    25

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 import "./App.css";
+import Cards from './features/covid/Cards/Cards';
 
-function App() {
-	return (<div></div>
-	)
+function App()
+{
+	return (
+		<div className="App">
+			<header className="App-header">
+				<Cards />
+			</header>
+		</div>
+  )
 };
-
 export default App;
